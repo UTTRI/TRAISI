78f423974c8c44c5742827169071f9f20e5e0e9d
Brendan Reilly <brendan.reilly@utoronto.ca>
2021-02-21 13:04:59 -0500

    Updated address error message

--- src/Traisi.Questions/src/travel-diary-scheduler/pipes/address-display.pipe.ts
+++ src/Traisi.Questions/src/travel-diary-scheduler/pipes/address-display.pipe.ts
@@ -1,9 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Address } from 'traisi-question-sdk';
 
-@Pipe({name: 'addressDisplay'}) 
+@Pipe({ name: 'addressDisplay' })
 export class AddressDisplayPipe implements PipeTransform {
-  transform(address: Address): string {
-    return `${address?.streetNumber} ${address?.streetAddress}, ${address?.city}`;
-  }
+	transform(address: Address): string {
+		if (!address) {
+			return '<address formatting error>';
+		}
+		return `${address?.streetNumber} ${address?.streetAddress}, ${address?.city}`;
+	}
 }
