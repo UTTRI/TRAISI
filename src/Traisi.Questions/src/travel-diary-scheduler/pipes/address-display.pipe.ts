484dd3c5916b06951a1bee32c5ecd0315b41de27
Brendan Reilly <brendan.reilly@utoronto.ca>
2021-02-24 14:05:53 -0500

    Address output to console from pipe

--- src/Traisi.Questions/src/travel-diary-scheduler/pipes/address-display.pipe.ts
+++ src/Traisi.Questions/src/travel-diary-scheduler/pipes/address-display.pipe.ts
@@ -4,6 +4,7 @@ import { Address } from 'traisi-question-sdk';
 @Pipe({ name: 'addressDisplay' })
 export class AddressDisplayPipe implements PipeTransform {
 	transform(address: Address): string {
+		console.log(address);
 		if (!address) {
 			return '<address formatting error>';
 		}
